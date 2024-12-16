5433be348c1e14b5743d37fbc70e79af6f009c67
ntsh <106670334+tffl@users.noreply.github.com>
2024-12-16 23:49:49 +0300

    feat: reverseString solution

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -266,8 +266,8 @@ function formatTime(minutes, seconds) {
  *   reverseString('abcdef') => 'fedcba'
  *   reverseString('12345') => '54321'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  return Array.from(str).reverse().join('');
 }
 
 /**
