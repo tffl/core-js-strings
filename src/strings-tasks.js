7dae6ca70fe95ee629107ddd7e3aba5b1417a7ed
ntsh <106670334+tffl@users.noreply.github.com>
2024-12-17 00:14:44 +0300

    feat: isPalindrome solution

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -339,8 +339,10 @@ function countVowels(str) {
  *   isPalindrome('apple') => false
  *   isPalindrome('No lemon, no melon') => true
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  const clearStr = str.toLowerCase().replace(/[.,\s!?]/g, '');
+  const reverse = Array.from(clearStr).reverse().join('');
+  return clearStr === reverse;
 }
 
 /**
